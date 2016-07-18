4e6ce1b71a8301ad432eb500122d9dbf1256848f
zmlfdogw <chinamichaelzhang@192.168.2.2>
2016-07-18 17:17:03 +0800

    add a vote button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'topics#index'
-  resources :topics
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718062013) do
+ActiveRecord::Schema.define(version: 20160718085847) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
